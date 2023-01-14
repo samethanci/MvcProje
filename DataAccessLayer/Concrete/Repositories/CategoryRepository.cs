6bae69cc3429a724480002865911e788043672eb
Samet HANCI <Samet HANCI@SAMET>
2023-01-14 15:26:46 +0300

    14/01/2023

---
+++ DataAccessLayer/Concrete/Repositories/CategoryRepository.cs
@@ -0,0 +1,44 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    public class CategoryRepository : ICategoryDal
+    {
+        Context context = new Context();
+        DbSet<Category> _categories;
+        public void Add(Category category)
+        {
+           _categories.Add(category);
+            context.SaveChanges();
+        }
+
+        public void Delete(Category category)
+        {
+            _categories.Remove(category);
+            context.SaveChanges();
+        }
+
+        public List<Category> List()
+        {
+            return _categories.ToList();
+        }
+
+        public List<Category> List(Expression<Func<Category, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category category)
+        {
+            context.SaveChanges();
+        }
+    }
+}
