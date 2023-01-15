f5ea4134b432afffd25e7b09062144bce0917c3f
Samet HANCI <Samet HANCI@SAMET>
2023-01-15 17:16:09 +0300

    15/01/2023

--- MvcProje/Controllers/CategoryController.cs
+++ MvcProje/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,16 @@ namespace MvcProje.Controllers
     public class CategoryController : Controller
     {
         // GET: Category
+
+        CategoryManager categoryManager = new CategoryManager();
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult GetCategoryList()
+        {
+            var categoryValues = categoryManager.GetAll();
+            return View(categoryValues);
         }
     }
 }
