63a0ee2a79f7689c250a2bdcae38a8d10f0233e7
Samet HANCI <Samet HANCI@SAMET>
2023-01-15 14:43:21 +0300

    15/01/2023

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,32 @@
+﻿using CoreLayer.DataAccess.EntityFramework;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        EfEntityRepositoryBase<Category, DatabaseContext> repositoryBase = new EfEntityRepositoryBase<Category, DatabaseContext>();
+
+        public List<Category> GetAll()
+        {
+            return repositoryBase.GetAll();
+        }
+        public void Add(Category category)
+        {
+            if (category.CategoryName == "" || category.CategoryName.Length <= 3 || category.CategoryDescription == "" || category.CategoryName.Length >= 51)
+            {
+                // hata mesajı
+            }
+            else
+            {
+                repositoryBase.Add(category);
+            }
+        }
+    }
+}
